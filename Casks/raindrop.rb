b5a0911ff2a98a1fd12c2cab322ef4db23dbd81f
mgm136044 <mkm0530@koreatech.ac.kr>
2026-04-07 02:20:13 +0900

    fix: update sha256 for v1.6.0 (cleanup + thread safety)

--- Casks/raindrop.rb
+++ Casks/raindrop.rb
@@ -1,6 +1,6 @@
 cask "raindrop" do
   version "1.6.0"
-  sha256 "a671adfec9dca78f04ec4d77373eac7e72f95b71acbf234da9a6dcf5f4727da4"
+  sha256 "f71d7183e0216d8551c604f087c00720d37eb385127b2fa52c17dfb04216e3d3"
 
   url "https://github.com/mgm136044/Raindrop/releases/download/v#{version}/RainDrop.zip"
   name "RainDrop"

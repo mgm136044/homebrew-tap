1eb822f8647e4d5f480a73c0450da522eb12fcbd
mgm136044 <mkm0530@koreatech.ac.kr>
2026-04-12 22:16:49 +0900

    bump raindrop to v2.8.2 (garden tab + water floor fix)

--- Casks/raindrop.rb
+++ Casks/raindrop.rb
@@ -1,6 +1,6 @@
 cask "raindrop" do
   version "2.8.2"
-  sha256 "ed065d9392c01cb3f400c9555126c163a49c8c742a30615314a64f2320be04af"
+  sha256 "84c9a0d55da95a81a8113f6f1d529dd63ce3877dd1da4c4b5c6387d01021de29"
 
   url "https://github.com/mgm136044/Raindrop/releases/download/v#{version}/RainDrop.zip"
   name "RainDrop"

269091641cfbbd15a08967e1bc56f45c1a415710
mgm136044 <mkm0530@koreatech.ac.kr>
2026-04-13 00:08:13 +0900

    bump raindrop v2.8.3 (wave + drain + click fixes)

--- Casks/raindrop.rb
+++ Casks/raindrop.rb
@@ -1,6 +1,6 @@
 cask "raindrop" do
   version "2.8.3"
-  sha256 "faa16605631499ecbdaf5aaa79aa57273c7590e336526f668396a21ac7a8a162"
+  sha256 "3adfd8e7f94389ca2debdb8b6bd0523c2475012e7fd0ddc5bde10a8a0c7919e0"
 
   url "https://github.com/mgm136044/Raindrop/releases/download/v#{version}/RainDrop.zip"
   name "RainDrop"

526c6620b2a612ebea38c06fc8b49c7a7bc843f0
mgm136044 <mkm0530@koreatech.ac.kr>
2026-04-13 05:09:42 +0900

    bump raindrop v2.8.3 (infinite mode + animation fixes)

--- Casks/raindrop.rb
+++ Casks/raindrop.rb
@@ -1,6 +1,6 @@
 cask "raindrop" do
   version "2.8.3"
-  sha256 "3adfd8e7f94389ca2debdb8b6bd0523c2475012e7fd0ddc5bde10a8a0c7919e0"
+  sha256 "653a3490d50485486b494a6250653058c710d3c9d46d190c78ca1108f56c78a4"
 
   url "https://github.com/mgm136044/Raindrop/releases/download/v#{version}/RainDrop.zip"
   name "RainDrop"
